e37857685e2d5c22128007c0bd79f9f0ed3e1453
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-06-07 17:25:27 +0200

    snack 1

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -1,4 +1,19 @@
+<!--
+  Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+
+  Olimpia Milano - Cantù | 55-60
+-->
+
+
+
 <?php 
+
+$Squadre=[ "Olimpia Milano - Cantù","Olimpia Milano - Cantù","Olimpia Milano - Cantù","Olimpia Milano - Cantù","Olimpia Milano - Cantù","Olimpia Milano - Cantù","Olimpia Milano - Cantù","Olimpia Milano - Cantù"];
+
+$Punteggio=[ 
+"55-60","55-60","55-70","55-60","55-60","50-60", "55-80", "55-60", "46-60",
+];
+
 
 
 ?>
@@ -13,6 +28,14 @@
 </head>
 <body>
     
+<h1>I RISULTATI SONO:</h1>
+
+<ul>
+  <?php for($i = 0; $i < 8; $i++){?>
+    <li><?php echo $Squadre[$i]; echo $Punteggio[$i]  ?></li> 
+  <?php } ?>
+
+</ul>
 
 </body>
 </html>
