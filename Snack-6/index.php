ecec3b66a7ef52002ab4928a55bc137c2a412986
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-06-08 12:58:21 +0200

    snack 6

--- Snack-6/index.php
+++ Snack-6/index.php
@@ -1,5 +1,31 @@
+<!--
+  Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+-->
+
 <?php 
 
+$db = [
+  'teachers' => [
+      [
+          'name' => 'Michele',
+          'lastname' => 'Papagni'
+      ],
+      [
+          'name' => 'Fabio',
+          'lastname' => 'Forghieri'
+      ]
+  ],
+  'pm' => [
+      [
+          'name' => 'Roberto',
+          'lastname' => 'Marazzini'
+      ],
+      [
+          'name' => 'Federico',
+          'lastname' => 'Pellegrini'
+      ]
+  ]
+];
 
 ?>
 
@@ -10,9 +36,33 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Snack 6</title>
+  <style>
+    div{
+      height: 500px;
+      width: 500px;
+      text-align: center;
+      vertical-align: center;
+    }
+
+    .teachers{
+      background-color: green;
+    }
+
+    .pm{
+      background-color: grey;
+    }
+
+  </style>
 </head>
 <body>
-    
-
+    <?php foreach($db as $role => $people): ?>
+      <div class="<?php echo $role ?>">
+        <ul>
+          <?php foreach($people as $person): ?>
+            <?php echo $person['name']; $person['lastname']; ?>
+            <?php endforeach ?>
+        </ul>
+      </div>
+      <?php endforeach ?>
 </body>
 </html>
